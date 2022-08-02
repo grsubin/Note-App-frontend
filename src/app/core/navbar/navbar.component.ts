f14a807bfff704c41222f8d920013094222d3384
grsubin <963subin@gmail.com>
2022-08-02 09:51:02 +0545

    nav bar

--- src/app/core/navbar/navbar.component.ts
+++ src/app/core/navbar/navbar.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { AuthenticationService } from '../_services/authentication.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent implements OnInit {
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private authService: AuthenticationService,
+    private toastr: ToastrService
+  ) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  onLogout() {
+    this.authService.logout();
+    this.toastr.success('Logged out');
+    this.router.navigateByUrl('/login');
   }
 
+  reloadPage(event) {
+    event.preventDefault();
+
+    this.router
+      .navigateByUrl('/', { skipLocationChange: true })
+      .then(() => this.router.navigate(['user/notes']));
+    console.log(this.router.url);
+  }
 }
