5dd5f4006ec9a2fddd41466b56f9f7140069851c
grsubin <963subin@gmail.com>
2022-08-02 09:49:18 +0545

    Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,21 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CoreComponent } from './core/core.component';
+import { UserGuard } from './core/_guards/user.guard';
 import { UserdetailsComponent } from './user/userdetails/userdetails.component';
 
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./authentication/authentication.module').then(m => m.AuthenticationModule)
+    loadChildren: () =>
+      import('./authentication/authentication.module').then(
+        (m) => m.AuthenticationModule
+      ),
   },
   {
     path: 'user',
-    loadChildren:() => import('./core/core.module').then(m => m.CoreModule)
-  }
+    canActivateChild: [UserGuard],
+    loadChildren: () => import('./core/core.module').then((m) => m.CoreModule),
+  },
+  {
+    path: '**',
+    canActivateChild: [UserGuard],
+    redirectTo: 'user',
+  },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes, { onSameUrlNavigation: 'reload' })],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/notes/notes-routing.module.ts
+++ src/app/notes/notes-routing.module.ts
@@ -1,22 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddNoteComponent } from './add-note/add-note.component';
 import { NoteDetailsComponent } from './note-details/note-details.component';
+import { NoteEditComponent } from './note-edit/note-edit.component';
 import { NotesListComponent } from './notes-list/notes-list.component';
 
 const routes: Routes = [
   {
-        path: '', component: NotesListComponent,
-        children: [
-          {
-            path: ':note-title', component: NoteDetailsComponent
-          }
-        ]
+    path: '',
+    component: NotesListComponent,
+    children: [
+      {
+        path: 'view/:note-title',
+        component: NoteDetailsComponent,
+      },
+      {
+        path: 'view/:note-title/edit',
+        component: NoteEditComponent,
+      },
+      {
+        path: 'add',
+        component: AddNoteComponent,
+      },
+    ],
   },
-
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class NotesRoutingModule { }
+export class NotesRoutingModule {}
